23c7179ca29c2c812d599f10f5e42d1c938a25d3
Regine Djalo <djalo50@gmail.com>
2020-11-19 18:02:45 +0100

    20201119

--- src/form/form.js
+++ src/form/form.js
@@ -2,13 +2,48 @@ import "../assets/style/styles.scss"
 import "./form.scss"
 
 const form = document.querySelector("form");
+let errors = [];
+const errorElement = document.querySelector("#errors");
 
-form.addEventListener("submit", event => {
-    event.preventDefault();
+form.addEventListener("submit", async e => {
+    e.preventDefault();
 
-    const formData = new formData(form);
-    const entries = formData.entries();
+    const formData = new FormData(form);
+    const article = Object.fromEntries(formData.entries());
+    if (formIsValid(article)) {
+        try {
+            const json = JSON.stringify(article);
+            const response = await fetch("https://restapi.fr/api/articlerd", {
+                method: "POST",
+                body: json,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            const body = await response.json();
+            console.log(body);
+        } catch (e) {
+            console.error("e : ", e);
+        }
+    }
+});
 
-    console.log(entries);
+const formIsValid = (article) => {
+    if (!article.author || !article.category || !article.content) {
+        errors.push('Vous devez renseignez tous les champs');
 
-})
+    } else {
+        errors = [];
+    }
+    if (errors.length) {
+        let errorHTML = ''
+        errors.forEach((e) => {
+            errorHTML += `<li> ${e} </li>`
+        })
+        errorElement.innerHTML = errorHTML;
+        return false;
+    } else {
+        errorElement.innerHTML = '';
+        return true;
+    }
+}
